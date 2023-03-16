7a28d3f9ff0fdce17ff16a6c99d53091da9321a4
ramana44 <c.krishnamurthy-ramanaik@hzdr.de>
2023-03-16 15:49:13 +0100

    CNN_VAE updated model saving active

--- models.py
+++ models.py
@@ -235,3 +235,65 @@ class CNN_VAE_FMNIST(nn.Module):
         z = self.fc3(z)
         #print('z.shape', z.shape)
         return self.decoder(z), mu, logvar
+
+
+class VAE_try_MRI(nn.Module):
+    def __init__(self, image_channels=3, h_dim=16*9*9, z_dim=4):
+        super(VAE_try_MRI, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, 3, stride=2, padding=1),  #N, 1, 32, 32
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 3, stride=2, padding=1),  #N, 1, 32, 32
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 3, stride=2, padding=1),  #N, 1, 32, 32
+            nn.ReLU(),
+            nn.Conv2d(64, 32, 5, stride=1, padding=1),  #N, 1, 32, 32
+            nn.ReLU(),
+            nn.Conv2d(32, 16, 4, stride=1, padding=1),  #N, 1, 32, 32
+            nn.ReLU(),
+            nn.Flatten(1,-1)
+            #nn.Linear(8*2*2, z_dim)
+
+        )
+        
+        self.fc1 = nn.Linear(h_dim, z_dim)
+        self.fc2 = nn.Linear(h_dim, z_dim)
+        self.fc3 = nn.Linear(z_dim, h_dim)
+        
+        self.decoder = nn.Sequential(
+            
+            #nn.Linear(z_dim, 8*2*2),
+            nn.Unflatten(1, (16, 9, 9)),
+            nn.ConvTranspose2d(16, 32, 4, stride=1, padding=1),   #N, 1, 32, 32
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 64, 5, stride=1, padding=1),   #N, 1, 32, 32
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1),   #N, 1, 32, 32
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1),   #N, 1, 32, 32
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1),   #N, 1, 32, 32
+            nn.Sigmoid()
+        )
+        
+    def reparameterize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        # return torch.normal(mu, std)
+        esp = torch.randn(*mu.size()).to(device)
+        z = mu + std * esp
+        return z.to(device)
+    
+    def bottleneck(self, h):
+        mu, logvar = self.fc1(h), self.fc2(h)
+        z = self.reparameterize(mu.to(device), logvar.to(device))
+        return z, mu, logvar
+        
+    def representation(self, x):
+        return self.bottleneck(self.encoder(x))[0]
+
+    def forward(self, x):
+        h = self.encoder(x)
+        z, mu, logvar = self.bottleneck(h.to(device))
+        z = self.fc3(z)
+        #print('z.shape', z.shape)
+        return self.decoder(z), mu, logvar
