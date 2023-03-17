b54c08f3faee0bfaaa0ae482be1222bb501265b8
ramana44 <c.krishnamurthy-ramanaik@hzdr.de>
2023-03-17 11:33:55 +0100

    mlp-vae updated

--- models.py
+++ models.py
@@ -431,3 +431,67 @@ class MLP_VAE_FMNIST(nn.Module):
         z = self.fc3(z)
         #print('z.shape', z.shape)
         return self.decoder(z), mu, logvar
+
+
+
+class VAE_mlp_MRI(nn.Module):
+    def __init__(self, image_size=96*96, h_dim=1000, z_dim=4):
+        super(VAE_mlp_MRI, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(image_size, 1000),  #input layer
+            nn.ReLU(),
+            nn.Linear(1000, 1000),   #h1
+            nn.ReLU(),
+            nn.Linear(1000, 1000),    #h1
+            nn.ReLU(),
+            nn.Linear(1000, 1000),    #h1
+            nn.ReLU(),
+            nn.Linear(1000, 1000),    #h1
+            nn.ReLU(),
+            nn.Linear(1000, 1000),    #h1
+            nn.ReLU()
+            #nn.Linear(100,z_dim)  # latent layer
+        )
+        
+        self.fc1 = nn.Linear(h_dim, z_dim)
+        self.fc2 = nn.Linear(h_dim, z_dim)
+        self.fc3 = nn.Linear(z_dim, h_dim)
+        
+        self.decoder = nn.Sequential(
+            #nn.Linear(z_dim, h_dim),  #input layer
+            #nn.ReLU(),
+            nn.Linear(1000, 1000),   #h1
+            nn.ReLU(),
+            nn.Linear(1000, 1000),   #h1
+            nn.ReLU(),
+            nn.Linear(1000, 1000),   #h1
+            nn.ReLU(),
+            nn.Linear(1000, 1000),    #h1
+            nn.ReLU(),
+            nn.Linear(1000, 1000),    #h1
+            nn.ReLU(),
+            nn.Linear(1000, image_size),  # latent layer
+            nn.Sigmoid()
+        )
+        
+    def reparameterize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        # return torch.normal(mu, std)
+        esp = torch.randn(*mu.size()).to(device)
+        z = mu + std * esp
+        return z.to(device)
+    
+    def bottleneck(self, h):
+        mu, logvar = self.fc1(h), self.fc2(h)
+        z = self.reparameterize(mu.to(device), logvar.to(device))
+        return z, mu, logvar
+        
+    def representation(self, x):
+        return self.bottleneck(self.encoder(x))[0]
+
+    def forward(self, x):
+        h = self.encoder(x)
+        z, mu, logvar = self.bottleneck(h.to(device))
+        z = self.fc3(z)
+        #print('z.shape', z.shape)
+        return self.decoder(z), mu, logvar
--- train_MLP_VAE_FMNIST_MRI/vae_mlp_9696MRI.py
+++ train_MLP_VAE_FMNIST_MRI/vae_mlp_9696MRI.py
@@ -3,41 +3,44 @@ sys.path.append('./')
 
 import torch
 import torch.nn.functional as F
+
 import os
-from models import MLP_VAE_FMNIST
+from models import VAE_mlp_MRI
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
 latent_dim = 10
 z_dim = latent_dim
-TDA = 0.4
-h_dim=100
-image_size=32*32
+TDA = 0.05
+h_dim=1000
+image_size=96*96
 lr = 0.0001
-no_layers = 3
+no_layers = 5
 no_epochs= 100
-hidden_size = 100
+hidden_size = 1000
+
+# the pelow parameters are the parameters(reg_nodes_sampling, deg_poly, alpha) of the correspondig AE-REG(regularized autoencoder) with which the 
+# MLP-VAE is going to be compared
+# These parameters are nowhere used in CNN-VAE, but only in the naming of CNN-VAE
+reg_nodes_sampling = 'legendre'
+deg_poly = 21
+alpha = 0.1
 
 
-
-
-model = MLP_VAE_FMNIST(image_size, h_dim, latent_dim).to(device)
-
+model = VAE_mlp_MRI(image_size, h_dim, latent_dim).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3) 
 
 def loss_fn(recon_x, x, mu, logvar):
     BCE = F.binary_cross_entropy(recon_x, x, size_average=False)
-
     KLD = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
-
     return BCE + KLD, BCE, KLD
 
 batch_size_cfs = 200
-image_batches_trn = torch.load('/home/ramana44/topological-analysis-of-curved-spaces-and-hybridization-of-autoencoders-STORAGE_SPACE/FMNIST_RK_coeffs/trainImages.pt').to(device)
-image_batches_trn = image_batches_trn.reshape(int(image_batches_trn.shape[0]/batch_size_cfs), batch_size_cfs, 1, 32,32)
-image_batches_test = torch.load('/home/ramana44/topological-analysis-of-curved-spaces-and-hybridization-of-autoencoders-STORAGE_SPACE/FMNIST_RK_coeffs/testImages.pt').to(device)
-image_batches_test = image_batches_test.reshape(int(image_batches_test.shape[0]/batch_size_cfs), batch_size_cfs, 1, 32,32)
+
+image_batches_trn = torch.load('/home/ramana44/topological-analysis-of-curved-spaces-and-hybridization-of-autoencoders-STORAGE_SPACE/savedDatasetAndCoeffs/trainDataSet.pt',map_location=torch.device('cuda'))
+image_batches_trn = image_batches_trn.reshape(int(image_batches_trn.shape[0]/batch_size_cfs), batch_size_cfs, 1, 96,96)
 image_batches_trn = image_batches_trn[:int(image_batches_trn.shape[0]*TDA)]
-image_batches_test = image_batches_test[:int(image_batches_test.shape[0]*TDA)]
 
 epochs = 50
 
@@ -45,8 +48,9 @@ for epoch in range(epochs):
     inum = 0
     for images in image_batches_trn:    
         inum = inum+1
-        images = images.reshape(-1, 32*32)
+        images = images.reshape(-1, 96*96)
 
+        this = model.encoder(images.to(device))
         recon_images, mu, logvar = model(images.to(device))
 
         loss, bce, kld = loss_fn(recon_images.to(device), images.to(device), mu.to(device), logvar.to(device))
@@ -54,13 +58,11 @@ for epoch in range(epochs):
         loss.backward()
         optimizer.step()
 
+
     print("Epoch : ", epoch)
 
 
 path = './models_saved/'
 os.makedirs(path, exist_ok=True)
-
-
 name = '_'+reg_nodes_sampling+'_'+'_'+str(TDA)+'_'+str(alpha)+'_'+str(hidden_size)+'_'+str(deg_poly)+'_'+str(latent_dim)+'_'+str(lr)+'_'+str(no_layers)
-torch.save(model.state_dict(), path+'/model_base_mlp_vae_TDA'+name)
-
+torch.save(model.state_dict(), path+'/model_base_mlp_vae_MRI'+name)
